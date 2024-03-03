324373bf53ad4cd537fe07c4d6b89784cbdcfe9a
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-03 20:05:51 +0300

    Take input from user

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -4,4 +4,13 @@ import java.util.Random;
 
 public class MineSweeper {
 
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Dimensions: ");
+        int row = scanner.nextInt();
+        int column = scanner.nextInt();
+
+        Minefield minefield = new Minefield(row, column);
+    }
 }
