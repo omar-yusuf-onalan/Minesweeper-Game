fc53e823c61beac04bffdffc1230a26d7735cadf
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-03 16:46:17 +0300

    Remove run method call and change name of MineSweeper object

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        MineSweeper mine = new MineSweeper();
-        mine.run();
+        MineSweeper mineSweeper = new MineSweeper();
     }
 }
